91eab7b79cc4c94af5e121c97de248b9ef14d8fe
willwalker7 <billywalker2128@gmail.com>
2026-09-08 20:50:39 +0200

    feat: add hotel booking prediction form

--- app/main.py
+++ app/main.py
@@ -1,27 +1,42 @@
-from fastapi import FastAPI, HTTPException, UploadFile, File
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, HTTPException, UploadFile, File, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 from app import model as ml
 from app.schemas import PredictionInput, PredictionOutput
 from app.prediction import predict
 
 app = FastAPI(
-    title= "Hotel Booking Cancellation Predictor API",
-    description= (
+    title="Hotel Booking Cancellation Predictor API",
+    description=(
         "API para predecir la probabilidad de cancelación de reservas de hoteles "
-        "basado en un modelo de ML(XGBoost) entrendo con el dataset de Kaggle 'Hotel Booking Demand'."
+        "basado en un modelo de ML (XGBoost) entrenado con el dataset de Kaggle "
+        "'Hotel Booking Demand'."
     ),
-    version= "0.0.1"
+    version="0.0.1"
 )
 
-@app.get("/")
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
 def root():
-    return RedirectResponse(url="/docs")
+    return """
+    <h2>🏨 Hotel Booking Cancellation Predictor</h2>
+    <p>Pulsa el botón para abrir el formulario de predicción.</p>
+    <a href="/predictor-form">
+        <button>Abrir formulario de predicción</button>
+    </a>
+    """
+
+
+@app.get("/predictor-form")
+def predictor_form(request: Request):
+    return templates.TemplateResponse(request, "index.html")
+
 
 @app.post("/predict", response_model=PredictionOutput)
-def make_prediction(data: PredictionInput):
-    # mostrar página.html con el formulario
-    return predict(data)
-
-@app.post("/app/v1/predict", response_model=PredictionOutput)
 def make_prediction(data: PredictionInput):
     return predict(data)
